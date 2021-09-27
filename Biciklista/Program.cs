5e4e086a97d9c34450830e00d6c41576f148ea21
usern4mehere9 <91462628+usern4mehere9@users.noreply.github.com>
2021-09-27 11:52:37 +0200

    Add files via upload

---
+++ Biciklista/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Biciklista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, v, v0, s;
+            v0 = double.Parse(Console.ReadLine());
+            a = double.Parse(Console.ReadLine());
+
+            s = v0 * 60 + ((a * 3600) / 2);
+            v = v0 + (a * 60);
+
+            Console.WriteLine(s);
+            Console.WriteLine(v);
+        }
+    }
+}
